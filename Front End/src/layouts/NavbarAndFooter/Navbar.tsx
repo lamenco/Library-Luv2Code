0860ba2b2cfd35e55077cbf56fcaec2d23f03f76
lamenco <40626373+lamenco@users.noreply.github.com>
2023-06-20 09:08:19 +0300

    Add files via upload

---
+++ Front End/src/layouts/NavbarAndFooter/Navbar.tsx
@@ -0,0 +1,43 @@
+import { NavLink } from "react-router-dom";
+import { useOktaAuth } from "@okta/okta-react";
+import { SpinnerLoading } from "../Utils/SpinnerLoaging";
+
+export const Navbar = () => {
+    const { oktaAuth, authState } = useOktaAuth();
+    if (!authState) {
+        return (<SpinnerLoading />)
+    }
+    return (
+        <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
+            <div className='container-fluid'>
+                <span className='navbar-brand'>Luv 2 Read</span>
+                <button className='navbar-toggler' type='button' data-bs-toggle='collapse'
+                    data-bs-target='#navbarNavDropdown'
+                    aria-controls='navbarNavDropdown' aria-expanded='false'
+                    aria-label='Toggle Navigation'>
+                    <span className='navbar-toggler-icon'></span>
+                </button>
+                <div className='collapse navbar-collapse' id='navbarNavDropdown'>
+                    <ul className='navbar-nav'>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to="/">Home </NavLink>
+                        </li>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to="/search">Search Books</NavLink>
+                        </li>
+                        {authState.isAuthenticated &&
+                            <li className="nav-item">
+                                <NavLink className='nav-link' to='/shelf'>Shelf</NavLink>
+                            </li>}
+                    </ul>
+                    <ul className='navbar-nav ms-auto'>
+                        <li className='nav-item m-1'>
+                            <a type='button' className='btn btn-outline-light' href="">Sign in</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+        ;
+}
